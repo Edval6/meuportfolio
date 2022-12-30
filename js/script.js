9421da04ef0c24654a797cd8d0ac79fea489959f
Edival Tembe <edvaltembe@gmail.com>
2022-12-30 13:08:56 +0200

    ao passar o mouse exibe o titulo do projeto

---
+++ js/script.js
@@ -0,0 +1,71 @@
+var im1 = document.querySelectorAll(".imagem")[0];
+
+im1.onmouseover = function(){
+    var titulo = document.querySelectorAll(".foto-texto")[0];
+    titulo.style.display = "block";
+}
+
+im1.onmouseout = function(){
+    var titulo = document.querySelectorAll(".foto-texto")[0];
+    titulo.style.display = "none";
+}
+
+var im2 = document.querySelectorAll(".imagem")[1];
+
+im2.onmouseover = function(){
+    var titulo = document.querySelectorAll(".foto-texto")[1];
+    titulo.style.display = "block";
+}
+
+im2.onmouseout = function(){
+    var titulo = document.querySelectorAll(".foto-texto")[1];
+    titulo.style.display = "none";
+}
+
+var im3 = document.querySelectorAll(".imagem")[2];
+
+im3.onmouseover = function(){
+    var titulo = document.querySelectorAll(".foto-texto")[2];
+    titulo.style.display = "block";
+}
+
+im3.onmouseout = function(){
+    var titulo = document.querySelectorAll(".foto-texto")[2];
+    titulo.style.display = "none";
+}
+
+var im4 = document.querySelectorAll(".imagem")[3];
+
+im4.onmouseover = function(){
+    var titulo = document.querySelectorAll(".foto-texto")[3];
+    titulo.style.display = "block";
+}
+
+im4.onmouseout = function(){
+    var titulo = document.querySelectorAll(".foto-texto")[3];
+    titulo.style.display = "none";
+}
+
+var im5 = document.querySelectorAll(".imagem")[4];
+
+im5.onmouseover = function(){
+    var titulo = document.querySelectorAll(".foto-texto")[4];
+    titulo.style.display = "block";
+}
+
+im5.onmouseout = function(){
+    var titulo = document.querySelectorAll(".foto-texto")[4];
+    titulo.style.display = "none";
+}
+
+var im6 = document.querySelectorAll(".imagem")[5];
+
+im6.onmouseover = function(){
+    var titulo = document.querySelectorAll(".foto-texto")[5];
+    titulo.style.display = "block";
+}
+
+im6.onmouseout = function(){
+    var titulo = document.querySelectorAll(".foto-texto")[5];
+    titulo.style.display = "none";
+}
