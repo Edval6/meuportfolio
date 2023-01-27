3550bf818e419c617dc7e442b3e6cd4aa444e23d
Edival Tembe <edvaltembe@gmail.com>
2023-01-27 16:36:27 +0200

    javascript para exercicio da ordem das vogais

---
+++ js/exercicio2vogais.js
@@ -0,0 +1,49 @@
+var vogalA = document.querySelector("#id-vogais-exe2");
+
+vogalA.onclick = function(){
+    this.style.color = "transparent";
+
+    var espacoA = document.querySelectorAll("#id-vogais-espaco")[0];
+    espacoA.style.display = "flex";
+    espacoA.style.color = "orange";
+}
+
+var vogalE = document.querySelector("#id-vogais-exe3");
+
+vogalE.onclick = function(){
+    this.style.color = "transparent";
+
+    var espacoE = document.querySelectorAll("#id-vogais-espaco")[1];
+    espacoE.style.display = "flex";
+    espacoE.style.color = "red";
+}
+
+var vogalI = document.querySelector("#id-vogais-exe4");
+
+vogalI.onclick = function(){
+    this.style.color = "transparent";
+
+    var espacoI = document.querySelectorAll("#id-vogais-espaco")[2];
+    espacoI.style.display = "flex";
+    espacoI.style.color = "blue";
+}
+
+var vogalO = document.querySelector("#id-vogais-exe5");
+
+vogalO.onclick = function(){
+    this.style.color = "transparent";
+
+    var espacoO = document.querySelectorAll("#id-vogais-espaco")[3];
+    espacoO.style.display = "flex";
+    espacoO.style.color = "yellow";
+}
+
+var vogalU = document.querySelector("#id-vogais-exe6");
+
+vogalU.onclick = function(){
+    this.style.color = "transparent";
+
+    var espacoU = document.querySelectorAll("#id-vogais-espaco")[4];
+    espacoU.style.display = "flex";
+    espacoU.style.color = "black";
+}
