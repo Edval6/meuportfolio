ab2248763887462367fea4f4227155468c131415
Edival Tembe <edvaltembe@gmail.com>
2023-01-27 12:51:02 +0200

    interacao com as vogais

--- js/exercicio1vogais.js
+++ js/exercicio1vogais.js
@@ -21,91 +21,201 @@ a1.onclick = function(){
                 o1.style.opacity = "25%";
                 o1.style.color = "green";
                 audExeA.src = audExeA.src="audios/vogais/e.mp3";
-            }
-        }
 
-        // letra o2
-        var o2 = document.querySelectorAll("#id-vogais-exe1")[5];
+                // letra e1
+                var e1 = document.querySelectorAll("#id-vogais-exe1")[1];
 
-        o2.onclick = function(){
-            const ovalue = "o";
+                e1.onclick = function(){
+                    const evalue = "e";
+        
+                    if(evalue === "e"){
+                        e1.style.opacity = "25%";
+                        e1.style.color = "green";
+                        audExeA.src = audExeA.src="audios/vogais/u.mp3";
 
-            if(ovalue === "o"){
-                o2.style.opacity = "25%";
-                o2.style.color = "green";
-                audExeA.src = audExeA.src="audios/vogais/e.mp3";
-            }
-        }
+                        //letra u1
+                        var u1 = document.querySelectorAll("#id-vogais-exe1")[4];
 
-        // letra o3
-        var o3 = document.querySelectorAll("#id-vogais-exe1")[14];
+                        u1.onclick = function(){
+                        const uvalue = "u";
 
-        o3.onclick = function(){
-            const ovalue = "o";
+                        if(uvalue === "u"){
+                            u1.style.opacity = "25%";
+                            u1.style.color = "green";
+                            audExeA.src = audExeA.src="audios/vogais/i.mp3";
+                        
 
-            if(ovalue === "o"){
-                o3.style.opacity = "25%";
-                o3.style.color = "green";
-                audExeA.src = audExeA.src="audios/vogais/e.mp3";
-            }
-        }
-    }
-    
-}
+                            // letra i1
+                            var i1 = document.querySelectorAll("#id-vogais-exe1")[2];
 
-//a2
+                            i1.onclick = function(){
+                                const ivalue = "i";
+                    
+                                if(ivalue === "i"){
+                                    i1.style.opacity = "25%";
+                                    i1.style.color = "green";
+                                    audExeA.src = audExeA.src="audios/vogais/u.mp3";
 
-var a1 = document.querySelectorAll("#id-vogais-exe1")[6];
+                                    // FIM DA PRIMEIRA LINHA
 
-a1.onclick = function(){
-    var audExeA = document.querySelector("#aud-exe-a");
-    const avalue = "a";
+                                    var maiusculas2 = document.querySelector("#maiusculas2");
+                                    maiusculas2.style.display = "flex";
 
-    if (avalue === "a"){
-        this.style.opacity = "40%";
-        audExeA.src = audExeA.src="audios/vogais/o.mp3";
+                                }
+                            }
 
-        var o1 = document.querySelectorAll("#id-vogais-exe1")[3];
+                            }
+                        }
 
-        o1.onclick = function(){
-            const ovalue = "o";
+                        //letra u2
+                        var u2 = document.querySelectorAll("#id-vogais-exe1")[7];
 
-            if(ovalue === "o"){
-                o1.style.opacity = "25%";
-                o1.style.color = "green";
-                audExeA.src = audExeA.src="audios/vogais/e.mp3";
-            }
-        }
+                        u2.onclick = function(){
+                        const uvalue = "u";
 
-    }
-    
-}
+                        if(uvalue === "u"){
+                            u2.style.opacity = "25%";
+                            u2.style.color = "green";
+                            audExeA.src = audExeA.src="audios/vogais/a.mp3";
+
+                            //letra a1
+                            var a1 = document.querySelectorAll("#id-vogais-exe1")[6];
+
+                            a1.onclick = function(){
+                                var audExeA = document.querySelector("#aud-exe-a");
+                                const avalue = "a";
+
+                                if (avalue === "a"){
+                                    this.style.opacity = "25%";
+                                    this.style.color = "green";
+                                    audExeA.src = audExeA.src="audios/vogais/i.mp3";
 
 
-//a3
-var a1 = document.querySelectorAll("#id-vogais-exe1")[11];
+                                    // letra i2
+                                    var i2 = document.querySelectorAll("#id-vogais-exe1")[8];
 
-a1.onclick = function(){
-    var audExeA = document.querySelector("#aud-exe-a");
-    const avalue = "a";
+                                    i2.onclick = function(){
+                                        const ivalue = "i";
+                            
+                                        if(ivalue === "i"){
+                                            i2.style.opacity = "25%";
+                                            i2.style.color = "green";
+                                            audExeA.src = audExeA.src="audios/vogais/e.mp3";
 
-    if (avalue === "a"){
-        this.style.opacity = "40%";
-        audExeA.src = audExeA.src="audios/vogais/o.mp3";
+                                             // letra e1
+                                            var e1 = document.querySelectorAll("#id-vogais-exe1")[9];
 
-        var o1 = document.querySelectorAll("#id-vogais-exe1")[3];
+                                            e1.onclick = function(){
+                                                const evalue = "e";
+                                    
+                                                if(evalue === "e"){
+                                                    e1.style.opacity = "25%";
+                                                    e1.style.color = "green";
+                                                    audExeA.src = audExeA.src="audios/vogais/o.mp3";
 
-        o1.onclick = function(){
-            const ovalue = "o";
+                                                     // letra o1
+                                                    var o2 = document.querySelectorAll("#id-vogais-exe1")[5];
 
-            if(ovalue === "o"){
-                o1.style.opacity = "25%";
-                o1.style.color = "green";
-                audExeA.src = audExeA.src="audios/vogais/e.mp3";
-            }
-        }
+                                                    o2.onclick = function(){
+                                                        const ovalue = "o";
 
-    }
-    
-}
+                                                        console.log(this);
 
+                                                        if(ovalue === "o"){
+                                                            o2.style.opacity = "25%";
+                                                            o2.style.color = "green";
+                                                            audExeA.src = audExeA.src="audios/vogais/i.mp3";
+
+                                                            // FIM DA SEGUNDA LINHA
+
+                                                            var maiusculas3 = document.querySelector("#maiusculas3");
+                                                            maiusculas3.style.display = "flex";
+
+                                                            // letra i3
+                                                            var i3 = document.querySelectorAll("#id-vogais-exe1")[10];
+
+                                                            i3.onclick = function(){
+                                                                const ivalue = "i";
+                                                    
+                                                                if(ivalue === "i"){
+                                                                    i3.style.opacity = "25%";
+                                                                    i3.style.color = "green";
+                                                                    audExeA.src = audExeA.src="audios/vogais/e.mp3";
+
+
+                                                                     // letra e1
+                                                                    var e3 = document.querySelectorAll("#id-vogais-exe1")[12];
+
+                                                                    e3.onclick = function(){
+                                                                        const evalue = "e";
+                                                            
+                                                                        if(evalue === "e"){
+                                                                            e3.style.opacity = "25%";
+                                                                            e3.style.color = "green";
+                                                                            audExeA.src = audExeA.src="audios/vogais/o.mp3";
+                                                                            
+
+
+                                                                            // letra o3
+                                                                            var o3 = document.querySelectorAll("#id-vogais-exe1")[14];
+
+                                                                            o3.onclick = function(){
+                                                                                const ovalue = "o";
+
+                                                                                console.log(this);
+
+                                                                                if(ovalue === "o"){
+                                                                                    o3.style.opacity = "25%";
+                                                                                    o3.style.color = "green";
+                                                                                    audExeA.src = audExeA.src="audios/vogais/u.mp3";
+
+                                                                                    //letra u3
+                                                                                    var u3 = document.querySelectorAll("#id-vogais-exe1")[11];
+
+                                                                                    u3.onclick = function(){
+                                                                                    const uvalue = "u";
+
+                                                                                    if(uvalue === "u"){
+                                                                                        u3.style.opacity = "25%";
+                                                                                        u3.style.color = "green";
+                                                                                        audExeA.src = audExeA.src="audios/vogais/a.mp3";
+
+                                                                                    //letra a1
+                                                                                    var a3 = document.querySelectorAll("#id-vogais-exe1")[13];
+
+                                                                                    a3.onclick = function(){
+                                                                                        var audExeA = document.querySelector("#aud-exe-a");
+                                                                                        const avalue = "a";
+
+                                                                                        if (avalue === "a"){
+                                                                                            this.style.opacity = "25%";
+                                                                                            this.style.color = "green";
+                                                                                            audExeA.style.display = "none";
+
+                                                                                        } 
+                                                                              }}     }
+                                                                          }
+                                                                      }}}
+                                                                  }
+                                                              }                          
+                                                                                     
+                                                      }
+                                                      
+                                                  }                     
+
+                                                        }
+                                                    }
+                                            }
+                                        }
+        
+                                        }
+                                        } 
+
+
+                                       } 
+                                  }
+                        }
+                    }}}
+                }
+            }                          
+                                   
