1d81cb7e4cc995e3b561e0cc919dc798add35647
Edival Tembe <edvaltembe@gmail.com>
2023-01-28 12:24:54 +0200

    organizacao das vogais de forma mais dinamica

--- js/exercicio2vogais.js
+++ js/exercicio2vogais.js
@@ -2,48 +2,277 @@ var vogalA = document.querySelector("#id-vogais-exe2");
 
 vogalA.onclick = function(){
     this.style.color = "transparent";
+    const vA = "A";
 
-    var espacoA = document.querySelectorAll("#id-vogais-espaco")[0];
-    espacoA.style.display = "flex";
-    espacoA.style.color = "orange";
+    var espacosA = document.querySelectorAll("#id-vogais-espaco")[0];
+    var espacoA = document.querySelectorAll("input")[0];
+    var x = espacoA.value;
+    //console.log(x);
+
+    if(x == ""){
+        espacoA.setAttribute('value', 'a');
+    }
+    if(x != ""){
+        var espacoE = document.querySelectorAll("input")[1];
+        var x = espacoE.value;
+
+        if(x == ""){
+            espacoE.setAttribute('value', 'a');
+        }
+
+        if(x != ""){
+            var espacoI = document.querySelectorAll("input")[2];
+            var x = espacoI.value;
+
+            if(x == ""){
+                espacoI.setAttribute('value', 'a');
+            }
+
+            if(x != ""){
+                var espacoO = document.querySelectorAll("input")[3];
+                var x = espacoO.value;
+    
+                if(x == ""){
+                    espacoO.setAttribute('value', 'a');
+                }
+
+                if(x != ""){
+                    var espacoU = document.querySelectorAll("input")[4];
+                    var x = espacoU.value;
+        
+                    if(x == ""){
+                        espacoU.setAttribute('value', 'a');
+                    }
+                }
+
+            }
+
+        }
+
+
+    }
+    /*espacosA.style.display = "flex";
+    espacosA.style.color = "orange";*/
 }
 
+// VOGAL E
 var vogalE = document.querySelector("#id-vogais-exe3");
 
 vogalE.onclick = function(){
     this.style.color = "transparent";
+    const vE = "E";
 
-    var espacoE = document.querySelectorAll("#id-vogais-espaco")[1];
-    espacoE.style.display = "flex";
-    espacoE.style.color = "red";
+    var espacoA = document.querySelectorAll("input")[0];
+    var x = espacoA.value;
+    //console.log(x);
+
+    if(x == ""){
+        espacoA.setAttribute('value', 'e');
+    }
+    if(x != ""){
+        var espacoE = document.querySelectorAll("input")[1];
+        var x = espacoE.value;
+
+        if(x == ""){
+            espacoE.setAttribute('value', 'e');
+        }
+
+        if(x != ""){
+            var espacoI = document.querySelectorAll("input")[2];
+            var x = espacoI.value;
+
+            if(x == ""){
+                espacoI.setAttribute('value', 'e');
+            }
+
+            if(x != ""){
+                var espacoO = document.querySelectorAll("input")[3];
+                var x = espacoO.value;
+    
+                if(x == ""){
+                    espacoO.setAttribute('value', 'e');
+                }
+
+                if(x != ""){
+                    var espacoU = document.querySelectorAll("input")[4];
+                    var x = espacoU.value;
+        
+                    if(x == ""){
+                        espacoU.setAttribute('value', 'e');
+                    }
+                }
+
+            }
+
+        }
+
+
+    }
 }
 
+
+// VOGAL I
 var vogalI = document.querySelector("#id-vogais-exe4");
 
 vogalI.onclick = function(){
     this.style.color = "transparent";
 
-    var espacoI = document.querySelectorAll("#id-vogais-espaco")[2];
-    espacoI.style.display = "flex";
-    espacoI.style.color = "blue";
+    var espacoA = document.querySelectorAll("input")[0];
+    var x = espacoA.value;
+    //console.log(x);
+
+    if(x == ""){
+        espacoA.setAttribute('value', 'i');
+    }
+    if(x != ""){
+        var espacoE = document.querySelectorAll("input")[1];
+        var x = espacoE.value;
+
+        if(x == ""){
+            espacoE.setAttribute('value', 'i');
+        }
+
+        if(x != ""){
+            var espacoI = document.querySelectorAll("input")[2];
+            var x = espacoI.value;
+
+            if(x == ""){
+                espacoI.setAttribute('value', 'i');
+            }
+
+            if(x != ""){
+                var espacoO = document.querySelectorAll("input")[3];
+                var x = espacoO.value;
+    
+                if(x == ""){
+                    espacoO.setAttribute('value', 'i');
+                }
+
+                if(x != ""){
+                    var espacoU = document.querySelectorAll("input")[4];
+                    var x = espacoU.value;
+        
+                    if(x == ""){
+                        espacoU.setAttribute('value', 'i');
+                    }
+                }
+
+            }
+
+        }
+
+
+    }
 }
 
+
+
+// VOGAL O
 var vogalO = document.querySelector("#id-vogais-exe5");
 
 vogalO.onclick = function(){
     this.style.color = "transparent";
 
-    var espacoO = document.querySelectorAll("#id-vogais-espaco")[3];
-    espacoO.style.display = "flex";
-    espacoO.style.color = "yellow";
+    var espacoA = document.querySelectorAll("input")[0];
+    var x = espacoA.value;
+    //console.log(x);
+
+    if(x == ""){
+        espacoA.setAttribute('value', 'o');
+    }
+    if(x != ""){
+        var espacoE = document.querySelectorAll("input")[1];
+        var x = espacoE.value;
+
+        if(x == ""){
+            espacoE.setAttribute('value', 'o');
+        }
+
+        if(x != ""){
+            var espacoI = document.querySelectorAll("input")[2];
+            var x = espacoI.value;
+
+            if(x == ""){
+                espacoI.setAttribute('value', 'o');
+            }
+
+            if(x != ""){
+                var espacoO = document.querySelectorAll("input")[3];
+                var x = espacoO.value;
+    
+                if(x == ""){
+                    espacoO.setAttribute('value', 'o');
+                }
+
+                if(x != ""){
+                    var espacoU = document.querySelectorAll("input")[4];
+                    var x = espacoU.value;
+        
+                    if(x == ""){
+                        espacoU.setAttribute('value', 'o');
+                    }
+                }
+
+            }
+
+        }
+
+
+    }
 }
 
+
+
+// VOGAL U
 var vogalU = document.querySelector("#id-vogais-exe6");
 
 vogalU.onclick = function(){
     this.style.color = "transparent";
 
-    var espacoU = document.querySelectorAll("#id-vogais-espaco")[4];
-    espacoU.style.display = "flex";
-    espacoU.style.color = "black";
+    var espacoA = document.querySelectorAll("input")[0];
+    var x = espacoA.value;
+    //console.log(x);
+
+    if(x == ""){
+        espacoA.setAttribute('value', 'u');
+    }
+    if(x != ""){
+        var espacoE = document.querySelectorAll("input")[1];
+        var x = espacoE.value;
+
+        if(x == ""){
+            espacoE.setAttribute('value', 'u');
+        }
+
+        if(x != ""){
+            var espacoI = document.querySelectorAll("input")[2];
+            var x = espacoI.value;
+
+            if(x == ""){
+                espacoI.setAttribute('value', 'u');
+            }
+
+            if(x != ""){
+                var espacoO = document.querySelectorAll("input")[3];
+                var x = espacoO.value;
+    
+                if(x == ""){
+                    espacoO.setAttribute('value', 'u');
+                }
+
+                if(x != ""){
+                    var espacoU = document.querySelectorAll("input")[4];
+                    var x = espacoU.value;
+        
+                    if(x == ""){
+                        espacoU.setAttribute('value', 'u');
+                    }
+                }
+
+            }
+
+        }
+
+
+    }
 }
