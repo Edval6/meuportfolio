f21613bb77e051aaf89f4a0414e86b78388e4a58
Edival Tembe <edvaltembe@gmail.com>
2023-01-30 14:01:22 +0200

    Estilo de Abecedario e Vogais

--- js/personalizado.js
+++ js/personalizado.js
@@ -1,10 +1,10 @@
 /* animacao das vogais */
 
 var Vogais = document.querySelectorAll("#id-vogais")[0];
+Vogais.style.color = "#d3e5f8";
 
 Vogais.addEventListener("click", function(){
     Vogais.style.animation = "girar 2s";
-    this.style.color = "orange";
 
     var audA = document.querySelectorAll("#audA")[0];
     audA.setAttribute('controls', 'true');
@@ -13,10 +13,10 @@ Vogais.addEventListener("click", function(){
 });
 
 var Vogais1 = document.querySelectorAll("#id-vogais")[1];
+Vogais1.style.color = "#d3e5f8";
 
     Vogais1.addEventListener("click", function(){
     Vogais1.style.animation = "girar 2s";
-    this.style.color = "pink";
 
     var audA = document.querySelectorAll("#audA")[1];
     audA.setAttribute('controls', 'true');
@@ -24,10 +24,10 @@ var Vogais1 = document.querySelectorAll("#id-vogais")[1];
 });
 
 var Vogais2 = document.querySelectorAll("#id-vogais")[2];
+Vogais2.style.color = "#d3e5f8";
 
     Vogais2.addEventListener("click", function(){
     Vogais2.style.animation = "girar 2s";
-    this.style.color = "#112BFA";
 
     var audA = document.querySelectorAll("#audA")[2];
     audA.setAttribute('controls', 'true');
@@ -35,10 +35,10 @@ var Vogais2 = document.querySelectorAll("#id-vogais")[2];
 });
 
 var Vogais3 = document.querySelectorAll("#id-vogais")[3];
+Vogais3.style.color = "#d3e5f8";
 
     Vogais3.addEventListener("click", function(){
     Vogais3.style.animation = "girar 2s";
-    this.style.color = "yellow";
 
     var audA = document.querySelectorAll("#audA")[3];
     audA.setAttribute('controls', 'true');
@@ -46,22 +46,255 @@ var Vogais3 = document.querySelectorAll("#id-vogais")[3];
 });
 
 var Vogais4 = document.querySelectorAll("#id-vogais")[4];
+Vogais4.style.color = "#d3e5f8";
 
     Vogais4.addEventListener("click", function(){
     Vogais4.style.animation = "girar 2s";
-    this.style.color = "red";
 
     var audA = document.querySelectorAll("#audA")[4];
     audA.setAttribute('controls', 'true');
     
 });
 
+var Vogais4 = document.querySelectorAll("#id-vogais")[5];
+Vogais4.style.color = "#d3e5f8";
 
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[5];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[6];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[6];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[7];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[7];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[8];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[8];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[9];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[9];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[10];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[10];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[11];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[11];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[12];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[12];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[13];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[13];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[14];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[14];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[15];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[15];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[16];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[16];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[17];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[17];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[18];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[18];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[19];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[19];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[20];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[20];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[21];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[21];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[22];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[22];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[23];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[23];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[24];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[24];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+var Vogais4 = document.querySelectorAll("#id-vogais")[25];
+Vogais4.style.color = "#d3e5f8";
+
+    Vogais4.addEventListener("click", function(){
+    Vogais4.style.animation = "girar 2s";
+
+    var audA = document.querySelectorAll("#audA")[25];
+    audA.setAttribute('controls', 'true');
+    
+});
+
+
+
+//minusculas
 var miniVogais = document.querySelectorAll("#id-mini-vogais")[0];
 
 miniVogais.addEventListener("click", function(){
     miniVogais.style.animation = "agitar 2s";
-    this.style.color = "orange";
+    this.style.color = "#d3e5f8";
 
     var audA = document.querySelectorAll("#audA")[5];
     audA.setAttribute('controls', 'true');
@@ -72,7 +305,7 @@ var miniVogais1 = document.querySelectorAll("#id-mini-vogais")[1];
 
 miniVogais1.addEventListener("click", function(){
     miniVogais1.style.animation = "agitar 2s";
-    this.style.color = "pink";
+    this.style.color = "#d3e5f8";
 
     var audA = document.querySelectorAll("#audA")[6];
     audA.setAttribute('controls', 'true');
@@ -94,7 +327,7 @@ var miniVogais3 = document.querySelectorAll("#id-mini-vogais")[3];
 
 miniVogais3.addEventListener("click", function(){
     miniVogais3.style.animation = "agitar 2s";
-    this.style.color = "yellow";
+    this.style.color = "#d3e5f8";
 
     var audA = document.querySelectorAll("#audA")[8];
     audA.setAttribute('controls', 'true');
@@ -105,7 +338,7 @@ var miniVogais4 = document.querySelectorAll("#id-mini-vogais")[4];
 
 miniVogais4.addEventListener("click", function(){
     miniVogais4.style.animation = "agitar 2s";
-    this.style.color = "red";
+    this.style.color = "#d3e5f8";
 
     var audA = document.querySelectorAll("#audA")[9];
     audA.setAttribute('controls', 'true');
